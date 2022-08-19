8f96c663b8f90c602c56e01b9551cc8d2eb5e208
David I. Jenei <jenei.david1994@gmail.com>
2022-08-19 15:50:22 +0200

    Add menu to frontend

---
+++ frontend/src/components/Layout/MenuBar.tsx
@@ -0,0 +1,37 @@
+import ALink from '../UI/ALink';
+import MenuItems from './MenuItems';
+
+import styles from './MenuBar.module.scss';
+import { useState } from 'react';
+import Button from '../UI/Button';
+
+const MenuBar = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isHamburgerMenuVisisble, setIsHamburgerMenuVisisble] = useState(false);
+
+  const hamburgerMenuHandler = () => {
+    setIsHamburgerMenuVisisble((prevState) => !prevState);
+  };
+
+  return (
+    <nav>
+      <div className={styles['menu-bar']}>
+        <ALink>WpA</ALink>
+        <MenuItems />
+        <Button
+          className={styles['hamburger-menu-icon']}
+          onClick={hamburgerMenuHandler}
+        >
+          [-]
+        </Button>
+      </div>
+      <div className={styles['hamburger-menu']}>
+        {isHamburgerMenuVisisble && (
+          <MenuItems isHamburgerMenu={isHamburgerMenuVisisble} />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default MenuBar;
