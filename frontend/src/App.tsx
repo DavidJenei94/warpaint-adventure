90a53b436b916aba9272d67426d5270998203384
David I. Jenei <jenei.david1994@gmail.com>
2022-08-19 15:50:43 +0200

    Testing App and Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Home from './pages/Home';
 
 import './App.scss';
+import MenuBar from './components/Layout/MenuBar';
 
 function App() {
   return (
     <div className="App">
+      <MenuBar />
       <Home />
     </div>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import styles from './Home.module.scss';
+
 const Home = () => {
-  return <>Home Page</>;
+  const [user4, setUser4]: any[] = useState();
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    const response = await fetch('http://localhost:4000/api/users/4');
+    const user4Data = await response.json();
+    setUser4(user4Data);
+  };
+  return (
+    <>
+      {user4 && <p>{user4.Email}</p>}
+      <p style={{ color: 'red' }}>Home asd Page refresh! </p>
+    </>
+  );
 };
 
 export default Home;
