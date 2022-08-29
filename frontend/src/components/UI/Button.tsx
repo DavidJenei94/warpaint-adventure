d01d33a5ca56efd88894f227db7c0e3d2691e830
David I. Jenei <jenei.david1994@gmail.com>
2022-08-29 19:32:13 +0200

    Edit basic UI elements

--- frontend/src/components/UI/Button.tsx
+++ frontend/src/components/UI/Button.tsx
@@ -1,12 +1,22 @@
 import { ComponentProps } from 'react';
+import { isUppercase } from '../../utils/general.utils';
 
 import styles from './Button.module.scss';
 
-const Button = ({ children, className, ...otherProps }: ComponentProps<'button'>) => {
+const Button = ({
+  children,
+  className,
+  ...otherProps
+}: ComponentProps<'button'>) => {
+  let adjClassName = '';
+  if (className) {
+    adjClassName = isUppercase(className) ? className : styles[className];
+  }
+
   return (
     <button
       {...otherProps}
-      className={`${className} ${styles['paral-button']}`}
+      className={`${adjClassName} ${styles['paral-button']}`}
     >
       {children}
     </button>
--- frontend/src/components/UI/Input.tsx
+++ frontend/src/components/UI/Input.tsx
@@ -1,10 +1,20 @@
 import { ComponentProps } from 'react';
+import { isUppercase } from '../../utils/general.utils';
 
 import styles from './Input.module.scss';
 
+const Input = ({ className, ...otherProps }: ComponentProps<'input'>) => {
+  let adjClassName = '';
+  if (className) {
+    adjClassName = isUppercase(className) ? className : styles[className];
+  }
 
-const Input = ({ ...otherProps }: ComponentProps<"input">) => {
-  return <input className={styles['paral-input']} {...otherProps}/>;
+  return (
+    <input
+      className={`${styles['paral-input']} ${adjClassName}`}
+      {...otherProps}
+    />
+  );
 };
 
 export default Input;
