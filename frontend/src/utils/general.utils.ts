015695d146550283fde003da6cee4b86c8021fe8
David I. Jenei <jenei.david1994@gmail.com>
2022-09-08 19:31:53 +0200

    Update utils

--- frontend/src/utils/general.utils.ts
+++ frontend/src/utils/general.utils.ts
@@ -1,26 +1,11 @@
-export const validateEmail = (email: string | number) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-    )
-    ? true
-    : false;
-};
+export const arraysEqual = (a: any[], b: any[]) => {
+  if (a === b) return true;
+  if (typeof a !== typeof b) return false;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
 
-export const validatePassword = (password: string) => {
-  const regex = new RegExp('^(?=.*?[a-zA-Z])(?=.*?[0-9]).{8,}$');
-  return regex.test(password);
-};
-
-export const validateName = (name: string) => {
-  const regex = new RegExp(
-    "^[w'-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[]]{2,}$"
-  );
-  return regex.test(name);
-};
-
-export const isUppercase = (word: string) => {
-  return /^\p{Lu}/u.test(word);
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 };
---
+++ frontend/src/utils/validation.utils.ts
@@ -0,0 +1,29 @@
+export const validateEmail = (email: string | number) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    )
+    ? true
+    : false;
+};
+
+export const validatePassword = (password: string) => {
+  const regex = new RegExp('^(?=.*?[a-zA-Z])(?=.*?[0-9]).{8,}$');
+  return regex.test(password);
+};
+
+export const validateName = (name: string) => {
+  return name.trim() !== '';
+
+  // fix this later
+  const regex = new RegExp(
+    "^[w'-,.][^0-9_!¡?÷?¿/\\+=@#$%^&*(){}|~<>;:[]]{2,}$"
+  );
+  return regex.test(name);
+};
+
+export const isUppercase = (word: string) => {
+  return /^\p{Lu}/u.test(word);
+};
