559ae6bfa13afba76629cda723dd72dca663b6e7
David I. Jenei <jenei.david1994@gmail.com>
2022-09-08 19:37:08 +0200

    Add error feedback and handling and update feedbackbar accordingly

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './auth';
+import feedbackReducer from './feedback';
 
-const store = configureStore({ reducer: { auth: authReducer } });
+const store = configureStore({
+  reducer: { auth: authReducer, feedback: feedbackReducer },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
